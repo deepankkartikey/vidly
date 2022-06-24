e81e2568b1db2e41e3e7ce04ee2f7e58ec61342c
Deepank kartikey <deepankkartikey@gmail.com>
2022-06-24 10:12:29 -0400

    Extract Routing and DB logic to seperate files

--- index.js
+++ index.js
@@ -4,17 +4,10 @@ require('winston-mongodb');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-
-const error = require('./middleware/error');
-const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+require('./startup/routes')(app);
+require('./startup/db')();
 
 process.on('uncaughtException', ex => {
 	// console.log('An uncaught exception!');
@@ -40,21 +33,6 @@ if (!config.get('jwtPrivateKey')) {
 	console.error('FATAL Error: jwtPrivateKey is not defined.');
 	process.exit(1);
 }
-
-mongoose
-	.connect('mongodb://localhost/vidly')
-	.then(() => console.log('Connected to MongoDB...'))
-	.catch(err => console.error('Could not connect to MongoDB...'));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-
-app.use(error); // error middleware
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
