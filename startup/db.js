a14815dc6aa9a6113e970842f11353274a3bd491
Deepank kartikey <deepankkartikey@gmail.com>
2022-06-28 16:21:41 -0400

    Setup test DB for integration tests

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,9 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = function () {
-	mongoose
-		.connect('mongodb://localhost/vidly')
-		.then(() => winston.info('Connected to MongoDB...'));
+	const db = config.get('db');
+	mongoose.connect(db).then(() => winston.info(`Connected to ${db} ...`));
 	// .catch(err => console.error('Could not connect to MongoDB...'));
 };
