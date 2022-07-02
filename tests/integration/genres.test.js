b8dc016ff1e838a6066715787fc5af95c10fc582
Deepank kartikey <deepankkartikey@gmail.com>
2022-07-02 13:45:30 -0400

    Added integration test for parameterized route

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -16,6 +16,8 @@ describe('/api/genres', () => {
 	afterEach(async () => {
 		server.close();
 		await Genre.remove({}); // clean up the db
+	});
+	afterAll(() => {
 		mongoose.connection.close();
 	});
 
@@ -32,6 +34,23 @@ describe('/api/genres', () => {
 			expect(res.body.length).toBe(2);
 			expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
 			expect(res.body.some(g => g.name === 'genre2')).toBeTruthy();
+		});
+	});
+
+	describe('GET /:id', () => {
+		it('should return genre if valid id is passed', async () => {
+			const genre = new Genre({ name: 'genre1' });
+			await genre.save();
+
+			const res = await request(server).get('/api/genres/' + genre._id);
+			expect(res.status).toBe(200);
+			expect(res.body).toHaveProperty('name', genre.name);
+		});
+
+		it('should return 404 if invalid id is passed', async () => {
+			const id = new mongoose.Types.ObjectId(); // generated random objectid
+			const res = await request(server).get('/api/genres/' + id);
+			expect(res.status).toBe(404);
 		});
 	});
 });
