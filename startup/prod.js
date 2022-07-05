fd6a6a678ef5f2a2eadc5dcadf70b48893f2d811
Deepank kartikey <deepankkartikey@gmail.com>
2022-07-05 00:05:12 -0400

    Typo, add start script

--- startup/prod.js
+++ startup/prod.js
@@ -2,6 +2,6 @@ const helmet = require('helmet');
 const compression = require('compression');
 
 module.exports = function (app) {
-	app.use(helemt());
+	app.use(helmet());
 	app.use(compression());
 };
